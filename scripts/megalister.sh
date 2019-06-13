1bdf7a8108b18a97abd82cf953ffee2da86b1dcd
atighineanu <alexei.tighineanu@suse.com>
2019-06-13 15:06:08 +0200

    test2

--- scripts/megalister.sh
+++ scripts/megalister.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-
-ls -alh some-files
+cp some-files/caasp-cluster.tf /app/caasp-openstack-terraform/caasp-cluster.tf
+cp some-files/terraform.tfvars /app/caasp-openstack-terraform/terraform.tfvars
+cp some-files/terraform.tfstate /app/caasp-openstack-terraform/terraform.tfstate
+/app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -createcaasp -caaspuiinst
+/app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -ostkcmd "terraform output" > some-files/out.json
