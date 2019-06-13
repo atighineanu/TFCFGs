2911e5c020b2f475ab7a44feae80d2ad9fbdb3cc
atighineanu <alexei.tighineanu@suse.com>
2019-06-13 12:24:29 +0200

    grinded func

--- scripts/megarunner.sh
+++ scripts/megarunner.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir -p some-files
-echo LALALA >> some-files/iaunti.txt
-echo triulealea >> some-files/doi.txt
+echo LALALA > some-files/iaunti.txt
+echo triulealea > some-files/doi.txt
