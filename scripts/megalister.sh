b4863558e05e7ad9dd330e779759934aaaaa2271
atighineanu <alexei.tighineanu@suse.com>
2019-06-13 15:36:08 +0200

    test improvements

--- scripts/megalister.sh
+++ scripts/megalister.sh
@@ -4,3 +4,7 @@ cp some-files/terraform.tfvars /app/caasp-openstack-terraform/terraform.tfvars
 cp some-files/terraform.tfstate /app/caasp-openstack-terraform/terraform.tfstate
 /app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -createcaasp -caaspuiinst
 /app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -ostkcmd "terraform output" > some-files/out.json
+cp /app/caasp-openstack-terraform/caasp-cluster.tf some-files/caasp-cluster.tf
+cp /app/caasp-openstack-terraform/terraform.tfvars some-files/terraform.tfvars
+cp /app/caasp-openstack-terraform/terraform.tfstate some-files/terraform.tfstate
+
