3862e2a33b3e6ddd4985d52504b2d40740206d74
atighineanu <alexei.tighineanu@suse.com>
2019-06-13 14:25:33 +0200

    grinded functionality

--- scripts/megarunner.sh
+++ scripts/megarunner.sh
@@ -1,5 +1,7 @@
-#!/bin/sh
+#/bin/sh
+/app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -createcaasp -action apply
+/app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -ostkcmd "terraform output" > some-files/out.json
+cp /app/caasp-openstack-terraform/caasp-cluster.tf some-files/caasp-cluster.tf
+cp /app/caasp-openstack-terraform/terraform.tfvars some-files/terraform.tfvars
+cp /app//caasp-openstack-terraform/terraform.tfstate some-files/terraform.tfstate
 
-#mkdir -p some-files
-echo LALALA 
-echo triulealea #> some-files/doi.txt
