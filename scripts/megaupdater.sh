611a651e45f4307ff0f88d56176baa15b2941f40
atighineanu <alexei.tighineanu@suse.com>
2019-06-14 12:05:09 +0200

    improved func

---
+++ scripts/megaupdater.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cp some-files/caasp-cluster.tf /app/caasp-openstack-terraform/caasp-cluster.tf
+cp some-files/terraform.tfvars /app/caasp-openstack-terraform/terraform.tfvars
+cp some-files/terraform.tfstate /app/caasp-openstack-terraform/terraform.tfstate
+/app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -reg
+/app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -dis
+/app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -ar $1
+/app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -cmd "zypper -n --gpg-auto-import-keys ref"
