4b9c690e34dbb5f50f036f1c180e7ee0c1c6b2c0
atighineanu <alexei.tighineanu@suse.com>
2019-06-14 09:59:02 +0200

    tests

--- scripts/megarunner.sh
+++ scripts/megarunner.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 /app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -createcaasp -action apply
-/app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -ostkcmd "terraform output" > some-files/out.json
+#/app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -ostkcmd "terraform output" > some-files/out.json
 cp /app/caasp-openstack-terraform/caasp-cluster.tf some-files/caasp-cluster.tf
 cp /app/caasp-openstack-terraform/terraform.tfvars some-files/terraform.tfvars
 cp /app/caasp-openstack-terraform/terraform.tfstate some-files/terraform.tfstate
