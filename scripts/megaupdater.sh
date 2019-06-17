06d81a4383b192d0ad077f338d5d7922d86e707c
atighineanu <alexei.tighineanu@suse.com>
2019-06-17 08:51:36 +0200

    tests

--- scripts/megaupdater.sh
+++ scripts/megaupdater.sh
@@ -3,6 +3,7 @@ cp some-files/caasp-cluster.tf /app/caasp-openstack-terraform/caasp-cluster.tf
 cp some-files/terraform.tfvars /app/caasp-openstack-terraform/terraform.tfvars
 cp some-files/terraform.tfstate /app/caasp-openstack-terraform/terraform.tfstate
 chmod 0500 some-files/id_caasp
+ls -alh some-files
 /app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -reg
 /app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -dis
 /app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -ar $1
