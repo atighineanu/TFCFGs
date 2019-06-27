fcd5731481121ce171c37b6526e426c40f93d2b6
atighineanu <alexei.tighineanu@suse.com>
2019-06-27 17:16:19 +0200

    grinded func

--- scripts/megalister.sh
+++ scripts/megalister.sh
@@ -3,7 +3,7 @@ ln -s /usr/bin/chromedriver /usr/sbin/chromedriver
 cp some-files/caasp-cluster.tf /app/caasp-openstack-terraform/caasp-cluster.tf
 cp some-files/terraform.tfvars /app/caasp-openstack-terraform/terraform.tfvars
 cp some-files/terraform.tfstate /app/caasp-openstack-terraform/terraform.tfstate
-go run /app/caasp/main.go -repo /app/caasp-openstack-terraform -auth openstack.json -caaspuiinst
+/app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -caaspuiinst
 #/app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -ostkcmd "terraform output" > some-files/out.json
 cp /app/caasp-openstack-terraform/caasp-cluster.tf some-files/caasp-cluster.tf
 cp /app/caasp-openstack-terraform/terraform.tfvars some-files/terraform.tfvars
