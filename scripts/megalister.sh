a4ae8bd558c06530ef9e6f5cc7474b516954c8f2
atighineanu <alexei.tighineanu@suse.com>
2019-06-28 15:06:49 +0200

    grinded func; added terraform refresh before starting velum stuff

--- scripts/megalister.sh
+++ scripts/megalister.sh
@@ -5,8 +5,10 @@ chmod 0500 /app/caasp-openstack-terraform/ssh/id_caasp
 cp some-files/caasp-cluster.tf /app/caasp-openstack-terraform/caasp-cluster.tf
 cp some-files/terraform.tfvars /app/caasp-openstack-terraform/terraform.tfvars
 cp some-files/terraform.tfstate /app/caasp-openstack-terraform/terraform.tfstate
+/app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -ostkcmd "terraform refresh"
 /app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -caaspuiinst
-#/app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -ostkcmd "terraform output" > some-files/out.json
 cp /app/caasp-openstack-terraform/caasp-cluster.tf some-files/caasp-cluster.tf
 cp /app/caasp-openstack-terraform/terraform.tfvars some-files/terraform.tfvars
 cp /app/caasp-openstack-terraform/terraform.tfstate some-files/terraform.tfstate
+cp /app/caasp-openstack-terraform/ssh/id_caasp some-files/id_caasp
+
--- scripts/megaupdater.sh
+++ scripts/megaupdater.sh
@@ -4,11 +4,11 @@ cp some-files/terraform.tfvars /app/caasp-openstack-terraform/terraform.tfvars
 cp some-files/terraform.tfstate /app/caasp-openstack-terraform/terraform.tfstate
 cp some-files/id_caasp /app/caasp-openstack-terraform/id_caasp
 chmod 0500 /app/caasp-openstack-terraform/id_caasp
-ls -alh some-files
 /app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -reg
 /app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -dis
 /app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -ar $1
 /app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -cmd "zypper -n --gpg-auto-import-keys ref"
 /app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -cmd "echo \"ZYPPER_AUTO_IMPORT_KEYS=1\" >> /etc/transactional-update.conf"
 /app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -sysupd
+sleep 10
 /app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -uiupd
