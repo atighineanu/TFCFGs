4cde3a5c9ca962b51398273376b4c9b49d054647
atighineanu <alexei.tighineanu@suse.com>
2019-07-01 08:41:58 +0200

    correction "flag option" -> flag option

--- scripts/healthchecks.sh
+++ scripts/healthchecks.sh
@@ -4,4 +4,4 @@ cp some-files/terraform.tfvars /app/caasp-openstack-terraform/terraform.tfvars
 cp some-files/terraform.tfstate /app/caasp-openstack-terraform/terraform.tfstate
 cp some-files/id_caasp /app/caasp-openstack-terraform/id_caasp
 chmod 0500 /app/caasp-openstack-terraform/id_caasp
-/app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -test "health"
+/app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -test health
