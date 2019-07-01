3de4b88164328b5897e9975af4367932485d1dcc
atighineanu <alexei.tighineanu@suse.com>
2019-07-01 08:39:41 +0200

    added healthchecks test

---
+++ scripts/healthchecks.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cp some-files/caasp-cluster.tf /app/caasp-openstack-terraform/caasp-cluster.tf
+cp some-files/terraform.tfvars /app/caasp-openstack-terraform/terraform.tfvars
+cp some-files/terraform.tfstate /app/caasp-openstack-terraform/terraform.tfstate
+cp some-files/id_caasp /app/caasp-openstack-terraform/id_caasp
+chmod 0500 /app/caasp-openstack-terraform/id_caasp
+/app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -test "health"
