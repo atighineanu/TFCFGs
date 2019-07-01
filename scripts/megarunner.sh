4e64833682cd780a132ff3cc4ef108527c13543a
fgerling <felix@fgerling.de>
2019-07-01 14:07:41 +0200

    Test different approach

--- scripts/megarunner.sh
+++ scripts/megarunner.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
-set -e
+function copy_some_files {
+	cp /app/caasp-openstack-terraform/caasp-cluster.tf some-files/caasp-cluster.tf
+	cp /app/caasp-openstack-terraform/terraform.tfvars some-files/terraform.tfvars
+	cp /app/caasp-openstack-terraform/terraform.tfstate some-files/terraform.tfstate
+	cp /app/caasp-openstack-terraform/ssh/id_caasp some-files/id_caasp
+	cat /app/caasp-openstack-terraform/terraform.tfstate
+}
+
 /app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -ostkcmd "terraform init"
+if [ $? -ne 0 ]; then
+	copy_some_files
+	return 1
+fi
+
 /app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -createcaasp -action apply
+if [ $? -ne 0 ]; then
+	copy_some_files
+	return 1
+fi
+copy_some_files
 #/app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -ostkcmd "terraform output" > some-files/out.json
-cp /app/caasp-openstack-terraform/caasp-cluster.tf some-files/caasp-cluster.tf
-cp /app/caasp-openstack-terraform/terraform.tfvars some-files/terraform.tfvars
-cp /app/caasp-openstack-terraform/terraform.tfstate some-files/terraform.tfstate
-cp /app/caasp-openstack-terraform/ssh/id_caasp some-files/id_caasp
-cat /app/caasp-openstack-terraform/terraform.tfstate
