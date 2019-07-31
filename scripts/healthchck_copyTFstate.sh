c74ae113af4c3e0512c49e4d1e7b9e0933c2a91a
atighineanu <alexei.tighineanu@suse.com>
2019-07-31 16:46:52 +0200

    File to export tfstate via 172.17.0.4:80 and continue with normal mkcaasp

---
+++ scripts/healthchck_copyTFstate.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cp some-files/caasp-cluster.tf /app/caasp-openstack-terraform/caasp-cluster.tf
+cp some-files/terraform.tfvars /app/caasp-openstack-terraform/terraform.tfvars
+cp some-files/terraform.tfstate /app/caasp-openstack-terraform/terraform.tfstate
+cp some-files/id_caasp /app/caasp-openstack-terraform/id_caasp
+chmod 0500 /app/caasp-openstack-terraform/id_caasp
+/app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -test health
+/app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -cmd "rpm -qi $1"
+/app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -ostkcmd "terraform output -json > some-files/terraform.tfstate"
+cp some-files/terraform.tfstate /var/www/localhost/htdocs/index.html
+exec /usr/sbin/httpd -D FOREGROUND -f /etc/apache2/httpd.conf
