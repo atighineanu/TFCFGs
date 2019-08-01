215d62abe8b9670e55c8556daa642f16e8c8e983
atighineanu <alexei.tighineanu@suse.com>
2019-08-01 11:41:54 +0200

    modified logic of tfoutput exposure

--- scripts/healthcheck_copyTFstate.sh
+++ scripts/healthcheck_copyTFstate.sh
@@ -2,10 +2,12 @@
 cp some-files/caasp-cluster.tf /app/caasp-openstack-terraform/caasp-cluster.tf
 cp some-files/terraform.tfvars /app/caasp-openstack-terraform/terraform.tfvars
 cp some-files/terraform.tfstate /app/caasp-openstack-terraform/terraform.tfstate
+cp some-files/terraform.tfstate /var/www/localhost/htdocs/index.html
 cp some-files/id_caasp /app/caasp-openstack-terraform/id_caasp
 chmod 0500 /app/caasp-openstack-terraform/id_caasp
 /app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -test health
 /app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -cmd "rpm -qi $1"
-/app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -ostkcmd "terraform output -json > some-files/terraform.tfstate"
-cp some-files/terraform.tfstate /var/www/localhost/htdocs/index.html
-exec /usr/sbin/httpd -D FOREGROUND -f /etc/apache2/httpd.conf
+echo "---begin---"
+/app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -ostkcmd "terraform output -json"
+echo "---end---"
+#exec /usr/sbin/httpd -D FOREGROUND -f /etc/apache2/httpd.conf
