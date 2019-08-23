9f67523e00f325500f21c2cae748fdc957ab671d
atighineanu <alexei.tighineanu@suse.com>
2019-08-23 11:14:07 +0200

    test

--- scripts/megarunnerV4.sh
+++ scripts/megarunnerV4.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-/root/go/bin/mkcaasp -v 4 -createcaasp -action apply -nodes w3m1
+/root/go/src/mkcaasp/main -v 4 -createcaasp -action apply -nodes w3m1
 #/app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -createcaasp -action apply
 echo "---end---"
 #/app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -ostkcmd "terraform output" > some-files/out.json
