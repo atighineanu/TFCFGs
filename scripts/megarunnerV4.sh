d4d53c6c8e95db63e2f8d2e641560ea938d467fd
atighineanu <alexei.tighineanu@suse.com>
2019-08-23 10:16:31 +0200

    first runner for CaaSP v4

---
+++ scripts/megarunnerV4.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+/app/mkcaasp -v 4 -createcaasp -action apply -nodes w3m1
+#/app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -createcaasp -action apply
+echo "---end---"
+#/app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -ostkcmd "terraform output" > some-files/out.json
+#cp /app/caasp-openstack-terraform/caasp-cluster.tf some-files/caasp-cluster.tf
+#cp /app/caasp-openstack-terraform/terraform.tfvars some-files/terraform.tfvars
+#cp /app/caasp-openstack-terraform/terraform.tfstate some-files/terraform.tfstate
+#cp /app/caasp-openstack-terraform/ssh/id_caasp some-files/id_caasp
+#cat /app/caasp-openstack-terraform/terraform.tfstate
