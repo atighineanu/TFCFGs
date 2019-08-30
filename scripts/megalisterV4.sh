352d0e9737d8a54bb174d1fd701b56cb76d342db
atighineanu <alexei.tighineanu@suse.com>
2019-08-30 11:38:51 +0200

    grinded to keep the imba-cluster folder state file (for skuba)

--- scripts/megalisterV4.sh
+++ scripts/megalisterV4.sh
@@ -7,3 +7,4 @@ ssh-add -k /root/go/src/mkcaasp/skuba_TF_stuff/id_shared
 /root/go/src/mkcaasp/main -v 4 -createcaasp -rpm -action init
 /root/go/src/mkcaasp/main -v 4 -createcaasp -action refresh -nodes w1m3 -rpm -bootstrap
 cp -r /usr/share/caasp/terraform/vmware/* other-files/
+cp -r /root/go/src/mkcaasp/tests/ginkgoscenarios/scenario1/imba-cluster other-files/
--- scripts/megaupdaterV4.sh
+++ scripts/megaupdaterV4.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 cp -r other-files/* /usr/share/caasp/terraform/vmware/
+cp -r some-files/imba-cluster /root/go/src/mkcaasp/tests/ginkgoscenarios/scenario1/
+
 eval $(ssh-agent)
 ssh-add -k /root/go/src/mkcaasp/skuba_TF_stuff/id_shared
 zypper up --auto-agree-with-licenses --no-confirm skuba
 cp -r /usr/share/caasp/terraform/vmware/* some-files/
+cp -r /root/go/src/mkcaasp/tests/ginkgoscenarios/scenario1/imba-cluster some-files/
