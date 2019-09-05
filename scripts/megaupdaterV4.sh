b368c4af49ea9b49cc0f8424124fef8b71ea19e3
atighineanu <alexei.tighineanu@suse.com>
2019-09-05 10:28:02 +0200

    grinded for zypper up instide nodes

--- scripts/megaupdaterV4.sh
+++ scripts/megaupdaterV4.sh
@@ -5,14 +5,17 @@ mkdir -p .kube/config
 cp /root/go/src/mkcaasp/tests/ginkgoscenarios/scenario1/imba-cluster/admin.conf .kube/config
 eval $(ssh-agent)
 ssh-add -k /root/go/src/mkcaasp/skuba_TF_stuff/id_shared
-/root/go/src/mkcaasp/main -v 4 -createcaasp -rpm -cmd "zypper -ar ${1}" -masters
-/root/go/src/mkcaasp/main -v 4 -createcaasp -rpm -cmd "zypper -ar ${1}" -workers
-/root/go/src/mkcaasp/main -v 4 -createcaasp -rpm -cmd "zypper -ar ${1}" -lb
+/root/go/src/mkcaasp/main -v 4 -createcaasp -action init
+/root/go/src/mkcaasp/main -v 4 -rpm -cmd "zypper -ar ${1}" -masters
+/root/go/src/mkcaasp/main -v 4 -rpm -cmd "zypper -ar ${1}" -workers
+/root/go/src/mkcaasp/main -v 4 -rpm -cmd "zypper -ar ${1}" -lb
 zypper up --auto-agree-with-licenses --no-confirm skuba
 zypper up --auto-agree-with-licenses --no-confirm
 sleep 10;
-/root/go/src/mkcaasp/main -v 4 -createcaasp -rpm -cmd "rpm -qi cri-o" -masters
-/root/go/src/mkcaasp/main -v 4 -createcaasp -rpm -cmd "rpm -qi cri-o" -workers
-/root/go/src/mkcaasp/main -v 4 -createcaasp -rpm -cmd "rpm -qi cri-o" -lb
+/root/go/src/mkcaasp/main -v 4 -rpm -cmd "rpm -qi cri-o" -masters
+/root/go/src/mkcaasp/main -v 4 -rpm -reboot masters
+/root/go/src/mkcaasp/main -v 4 -rpm -cmd "rpm -qi cri-o" -workers
+/root/go/src/mkcaasp/main -v 4 -rpm -reboot workers
+#/root/go/src/mkcaasp/main -v 4 -rpm -cmd "rpm -qi cri-o" -lb
 cp -r /usr/share/caasp/terraform/vmware/* some-files/
 cp -r /root/go/src/mkcaasp/tests/ginkgoscenarios/scenario1/imba-cluster some-files/
