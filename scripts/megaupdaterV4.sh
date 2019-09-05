cced655ecca4b3e72db06a714e95a32c8b13b26e
atighineanu <alexei.tighineanu@suse.com>
2019-09-05 10:06:04 +0200

    grinded for rpm checks

--- scripts/megaupdaterV4.sh
+++ scripts/megaupdaterV4.sh
@@ -5,10 +5,14 @@ mkdir -p .kube/config
 cp /root/go/src/mkcaasp/tests/ginkgoscenarios/scenario1/imba-cluster/admin.conf .kube/config
 eval $(ssh-agent)
 ssh-add -k /root/go/src/mkcaasp/skuba_TF_stuff/id_shared
-zypper -ar $1
+/root/go/src/mkcaasp/main -v 4 -createcaasp -rpm -cmd "zypper -ar ${1}" -masters
+/root/go/src/mkcaasp/main -v 4 -createcaasp -rpm -cmd "zypper -ar ${1}" -workers
+/root/go/src/mkcaasp/main -v 4 -createcaasp -rpm -cmd "zypper -ar ${1}" -lb
 zypper up --auto-agree-with-licenses --no-confirm skuba
-zypper up --auto-agree-with-licenses --no-confirm cri-o
+zypper up --auto-agree-with-licenses --no-confirm
 sleep 10;
-rpm -qi cri-o | egrep -i "version|release"
+/root/go/src/mkcaasp/main -v 4 -createcaasp -rpm -cmd "rpm -qi cri-o" -masters
+/root/go/src/mkcaasp/main -v 4 -createcaasp -rpm -cmd "rpm -qi cri-o" -workers
+/root/go/src/mkcaasp/main -v 4 -createcaasp -rpm -cmd "rpm -qi cri-o" -lb
 cp -r /usr/share/caasp/terraform/vmware/* some-files/
 cp -r /root/go/src/mkcaasp/tests/ginkgoscenarios/scenario1/imba-cluster some-files/
