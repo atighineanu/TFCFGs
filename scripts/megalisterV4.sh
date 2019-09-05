61bfbd0889c33e295f6334e4cb119346770b0383
atighineanu <alexei.tighineanu@suse.com>
2019-09-05 09:02:55 +0200

    smaller cluster

--- scripts/megalisterV4.sh
+++ scripts/megalisterV4.sh
@@ -4,6 +4,6 @@ cp -r some-files/* /usr/share/caasp/terraform/vmware/
 eval $(ssh-agent)
 ssh-add -k /root/go/src/mkcaasp/skuba_TF_stuff/id_shared
 /root/go/src/mkcaasp/main -v 4 -createcaasp -rpm -action init
-/root/go/src/mkcaasp/main -v 4 -createcaasp -action refresh -nodes w1m3 -rpm -bootstrap
+/root/go/src/mkcaasp/main -v 4 -createcaasp -action refresh -nodes w1m1 -rpm -bootstrap
 cp -r /usr/share/caasp/terraform/vmware/* other-files/
 cp -r /root/go/src/mkcaasp/tests/ginkgoscenarios/scenario1/imba-cluster other-files/
