83c99b1e58df61d36af2a3b67982b72571524b9e
atighineanu <alexei.tighineanu@suse.com>
2019-10-11 11:54:59 +0200

    added ssh-add

--- scripts/megalisterV4.sh
+++ scripts/megalisterV4.sh
@@ -3,7 +3,12 @@ cp -r some-files/* /usr/share/caasp/terraform/vmware/
 
 eval $(ssh-agent)
 ssh-add -k /root/go/src/mkcaasp/skuba_TF_stuff/id_shared
-/root/go/src/mkcaasp/main -v 4 -createcaasp -rpm -action init
-/root/go/src/mkcaasp/main -v 4 -createcaasp -action refresh -rpm -bootstrap
+/root/go/src/mkcaasp/main -v 4 -rpm -createcaasp -action init
+eval $(ssh-agent)
+ssh-add -k /root/go/src/mkcaasp/skuba_TF_stuff/id_shared
+/root/go/src/mkcaasp/main -v 4 -rpm -createcaasp -action refresh
+eval $(ssh-agent)
+ssh-add -k /root/go/src/mkcaasp/skuba_TF_stuff/id_shared
+/root/go/src/mkcaasp/main -v 4 -rpm -bootstrap
 cp -r /usr/share/caasp/terraform/vmware/* other-files/
 cp -r /root/go/src/mkcaasp/tests/ginkgoscenarios/scenario1/imba-cluster other-files/
