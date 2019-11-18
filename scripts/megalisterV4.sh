0c02b694bc177c20a01409a77ac2dfa7c05974b9
atighineanu <alexei.tighineanu@suse.com>
2019-11-18 12:42:36 +0100

    corrected path of mkcaasp exe

--- scripts/megalisterV4.sh
+++ scripts/megalisterV4.sh
@@ -12,8 +12,8 @@ sleep 5
 eval $(ssh-agent)
 ssh-add -k /root/go/src/mkcaasp/skuba_TF_stuff/id_shared
 sleep 3
-/root/go/src/mkcaasp/main.go -v 4 -rpm -ginkgo deploy01
+/root/go/src/mkcaasp/main -v 4 -rpm -ginkgo deploy01
 sleep 1
-/root/go/src/mkcaasp/main.go -v 4 -rpm -ginkgo add_cluster_nodes03
+/root/go/src/mkcaasp/main -v 4 -rpm -ginkgo add_cluster_nodes03
 cp -r /usr/share/caasp/terraform/vmware/* other-files/
 cp -r /root/go/src/mkcaasp/tests/ginkgoscenarios/scenario1/imba-cluster other-files/
