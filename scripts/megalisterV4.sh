c4e62f42ded0812dc83d439c3b3132021a103b65
Alexei Tighineanu <atighineanu@suse.de>
2020-03-17 16:32:55 +0100

    added updating of OS

--- scripts/megalisterV4.sh
+++ scripts/megalisterV4.sh
@@ -3,15 +3,19 @@ cp -r some-files/* /usr/share/caasp/terraform/vmware/
 zypper -n up skuba
 eval $(ssh-agent)
 ssh-add -k /root/go/src/mkcaasp/skuba_TF_stuff/id_shared
-/root/go/src/mkcaasp/main -v 4 -rpm -createcaasp -action init
+/root/go/src/mkcaasp/main -v 4 -rpm -vmware -createcaasp -action init
 sleep 5
 eval $(ssh-agent)
 ssh-add -k /root/go/src/mkcaasp/skuba_TF_stuff/id_shared
-/root/go/src/mkcaasp/main -v 4 -rpm -createcaasp -action refresh
+/root/go/src/mkcaasp/main -v 4 -rpm -vmware -createcaasp -action refresh
 sleep 10
 eval $(ssh-agent)
 ssh-add -k /root/go/src/mkcaasp/skuba_TF_stuff/id_shared
 sleep 3
+/root/go/src/mkcaasp/main -v 4 -rpm -vmware -cmd "sudo zypper ref; sudo zypper -n up; sudo systemctl disable firewalld; sudo reboot now" -all
+sleep 60
+eval $(ssh-agent)
+ssh-add -k /root/go/src/mkcaasp/skuba_TF_stuff/id_shared
 /root/go/src/mkcaasp/main -v 4 -rpm -ginkgo deploy01
 sleep 10
 eval $(ssh-agent)
