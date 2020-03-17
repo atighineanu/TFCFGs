d2b035aa230590f1df286e4733ac2e935c085bde
Alexei Tighineanu <atighineanu@suse.de>
2020-03-18 00:18:13 +0100

    Update megatester.sh

--- scripts/megatester.sh
+++ scripts/megatester.sh
@@ -12,9 +12,9 @@ ssh-add -k /root/go/src/mkcaasp/skuba_TF_stuff/id_shared
 sleep 2
 kubectl get nodes
 sleep 2
-kubectl get services
+kubectl get all -n kube-system
 sleep 2
-/root/go/src/mkcaasp/main -v 4 -rpm -reboot masters
+/root/go/src/mkcaasp/main -v 4 -rpm -reboot workers
 sleep 2
 sleep 100
 /root/go/src/mkcaasp/main -v 4 -rpm -status
