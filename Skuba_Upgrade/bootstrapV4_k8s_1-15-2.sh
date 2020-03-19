aaaeb8ab05d5ea666b345cb25e41abea1e8c4344
atighineanu <atighineanu@suse.de>
2020-03-19 19:44:02 +0100

    fixed -rpm flag; removed updating of skuba

--- Skuba_Upgrade/bootstrapV4_k8s_1-15-2.sh
+++ Skuba_Upgrade/bootstrapV4_k8s_1-15-2.sh
@@ -2,14 +2,13 @@
 export MKCAASPROOT=/root/go/src/mkcaasp
 cp -r some-files/* /root/go/src/github.com/SUSE/skuba/ci/infra/vmware/
 cp some-files/mkcaaspcfg.json /root/go/src/mkcaasp/
-zypper -n up skuba
 eval $(ssh-agent)
 ssh-add -k /root/go/src/mkcaasp/skuba_TF_stuff/id_shared
-go run /root/go/src/mkcaasp/caasp/main.go -v 4 -rpm -vmware -createcaasp -action init
+go run /root/go/src/mkcaasp/caasp/main.go -v 4 -vmware -createcaasp -action init
 sleep 5
 eval $(ssh-agent)
 ssh-add -k /root/go/src/mkcaasp/skuba_TF_stuff/id_shared
-go run /root/go/src/mkcaasp/caasp/main.go -v 4 -rpm -vmware -createcaasp -action refresh
+go run /root/go/src/mkcaasp/caasp/main.go -v 4 -vmware -createcaasp -action refresh
 sleep 10
 eval $(ssh-agent)
 ssh-add -k /root/go/src/mkcaasp/skuba_TF_stuff/id_shared
@@ -17,7 +16,7 @@ sleep 3
 eval $(ssh-agent)
 ssh-add -k /root/go/src/mkcaasp/skuba_TF_stuff/id_shared
 export GO111MODULE=on
-go run /root/go/src/mkcaasp/caasp/main.go -v 4 -rpm -k8sversion 1.15.2 -ginkgo deploy01
+go run /root/go/src/mkcaasp/caasp/main.go -v 4 -k8sversion 1.15.2 -ginkgo deploy01
 sleep 1
 cp /root/go/src/mkcaasp/mkcaaspcfg.json other-files/
 cp -r /root/go/src/github.com/SUSE/skuba/ci/infra/vmware/* other-files/
