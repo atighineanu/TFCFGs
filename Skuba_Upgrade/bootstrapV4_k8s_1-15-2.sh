eeb038f8612f1e8d0ad7498859d44631041ef3ba
atighineanu <atighineanu@suse.de>
2020-03-19 22:07:23 +0100

    fixed calling

--- Skuba_Upgrade/bootstrapV4_k8s_1-15-2.sh
+++ Skuba_Upgrade/bootstrapV4_k8s_1-15-2.sh
@@ -4,12 +4,12 @@ cp -r some-files/* /root/go/src/github.com/SUSE/skuba/ci/infra/vmware/
 cp some-files/mkcaaspcfg.json /root/go/src/mkcaasp/
 eval $(ssh-agent)
 ssh-add -k /root/go/src/mkcaasp/skuba_TF_stuff/id_shared
-cd /root/go/src/mkcaasp
-go run caasp/main.go -v 4 -vmware -createcaasp -action init
+
+/root/go/src/mkcaasp/main -v 4 -vmware -createcaasp -action init
 sleep 5
 eval $(ssh-agent)
 ssh-add -k /root/go/src/mkcaasp/skuba_TF_stuff/id_shared
-go run caasp/main.go -v 4 -vmware -createcaasp -action refresh
+/root/go/src/mkcaasp/main -v 4 -vmware -createcaasp -action refresh
 sleep 10
 eval $(ssh-agent)
 ssh-add -k /root/go/src/mkcaasp/skuba_TF_stuff/id_shared
@@ -17,7 +17,8 @@ sleep 3
 eval $(ssh-agent)
 ssh-add -k /root/go/src/mkcaasp/skuba_TF_stuff/id_shared
 export GO111MODULE=on
-go run caasp/main.go -v 4 -k8sversion 1.15.2 -ginkgo deploy01
+export GOPATH=/root/go
+/root/go/src/mkcaasp/main -v 4 -k8sversion 1.15.2 -ginkgo deploy01
 cp /root/go/src/mkcaasp/mkcaaspcfg.json other-files/
 cp -r /root/go/src/github.com/SUSE/skuba/ci/infra/vmware other-files/vmware
 cp -r /root/go/src/mkcaasp/tests/ginkgoscenarios/imba-cluster other-files/imba-cluster
