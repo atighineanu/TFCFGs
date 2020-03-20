4f260d8c3a8e0a5b4af2f4dce07004f4788241d9
atighineanu <atighineanu@suse.de>
2020-03-20 01:12:25 +0100

    fixed execution and copying of state files

--- Skuba_Upgrade/bootstrapV4_k8s_1-15-2.sh
+++ Skuba_Upgrade/bootstrapV4_k8s_1-15-2.sh
@@ -4,7 +4,6 @@ cp -r some-files/* /root/go/src/github.com/SUSE/skuba/ci/infra/vmware/
 cp some-files/mkcaaspcfg.json /root/go/src/mkcaasp/
 eval $(ssh-agent)
 ssh-add -k /root/go/src/mkcaasp/skuba_TF_stuff/id_shared
-
 /root/go/src/mkcaasp/main -v 4 -vmware -createcaasp -action init
 sleep 5
 eval $(ssh-agent)
@@ -18,7 +17,7 @@ eval $(ssh-agent)
 ssh-add -k /root/go/src/mkcaasp/skuba_TF_stuff/id_shared
 export GO111MODULE=on
 export GOPATH=/root/go
-/root/go/src/mkcaasp/main -v 4 -k8sversion 1.15.2 -ginkgo deploy01
+/root/go/src/mkcaasp/main -v 4 -k8sversion 1.15.2 -bootstrap
 cp /root/go/src/mkcaasp/mkcaaspcfg.json other-files/
-cp -r /root/go/src/github.com/SUSE/skuba/ci/infra/vmware other-files/vmware
-cp -r /root/go/src/mkcaasp/tests/ginkgoscenarios/imba-cluster other-files/imba-cluster
+cp -r /root/go/src/github.com/SUSE/skuba/ci/infra/vmware/* other-files/
+cp -r /root/go/src/mkcaasp/tests/ginkgoscenarios/imba-cluster other-files/
