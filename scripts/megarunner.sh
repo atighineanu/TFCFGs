5d10377cfb943d2a571f7d337d12daee5375ca1a
Alexei Tighineanu <atighineanu@suse.de>
2020-04-09 17:58:04 +0200

    Update megarunner.sh
    upgraded skuba and TF

--- scripts/megarunner.sh
+++ scripts/megarunner.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+zypper -n in terraform
+zypper -n up skuba
 /app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -ostkcmd "terraform init"
 /app/mkcaasp -repo /app/caasp-openstack-terraform -auth openstack.json -createcaasp -action apply
 echo "---end---"
