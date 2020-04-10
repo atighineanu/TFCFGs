e81e556d11ce3804a655cc44fd83023bca38336a
Alexei Tighineanu <atighineanu@suse.de>
2020-04-10 12:36:18 +0200

    changed to openstack

--- scripts/megaupdaterV4.sh
+++ scripts/megaupdaterV4.sh
@@ -2,12 +2,12 @@
 export MKCAASPROOT=/root/go/src/mkcaasp
 export GO111MODULE=on
 cp other-files/mkcaaspcfg.json /root/go/src/mkcaasp/
-cp -r other-files/* /usr/share/caasp/terraform/vmware/
+cp -r other-files/* /usr/share/caasp/terraform/openstack/
 cp -r other-files/imba-cluster /root/go/src/mkcaasp/tests/ginkgoscenarios/deploy01/
 export KUBECONFIG=/root/go/src/mkcaasp/tests/ginkgoscenarios/deploy01/imba-cluster/admin.conf
 eval $(ssh-agent)
 ssh-add -k /root/go/src/mkcaasp/skuba_TF_stuff/id_shared
-/root/go/src/mkcaasp/main -v 4 -rpm -createcaasp -action init
+/root/go/src/mkcaasp/main -v 4 -openstack -rpm -createcaasp -action init
 echo REPO: "${1}"
 echo PACKAGE: "${2}"
 /root/go/src/mkcaasp/main -v 4 -rpm -cmd "sudo zypper ar ${1}" -all
@@ -19,5 +19,5 @@ sleep 10;
 sleep 3;
 /root/go/src/mkcaasp/main -v 4 -rpm -cmd "rpm -qi ${2}" -all
 /root/go/src/mkcaasp/main -v 4 -rpm -ginkgo check_container_versions02
-cp -r /usr/share/caasp/terraform/vmware/* some-files/
+cp -r /usr/share/caasp/terraform/vopenstack/* some-files/
 cp -r /root/go/src/mkcaasp/tests/ginkgoscenarios/scenario1/imba-cluster some-files/
